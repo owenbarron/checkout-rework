b1a8b8b177f6d97ab5870d247f70c35d4be97b0c
Owen Barron <owenbarron@owens-laptop.lan>
2026-04-17 11:13:57 -0400

    Add selectable checkout success chimes

--- fullheight/sw.js
+++ fullheight/sw.js
@@ -5,8 +5,11 @@ const ASSETS = [
     './manifest.json',
     '../images/USEFULL-Icon-Registered_Color.svg',
     '../sounds/successful-scan.m4a',
-    '../sounds/checkout-complete.mp3',
     '../sounds/error.mp3',
+    '../sounds/final-success-chime-1.mp3',
+    '../sounds/final-success-chime-2.mp3',
+    '../sounds/final-success-chime-3.mp3',
+    '../sounds/final-success-chime-4.mp3',
 ];
 
 self.addEventListener('install', (event) => {
